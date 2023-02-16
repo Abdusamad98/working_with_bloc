a75290c822dcff129f9814ba6ecc047ffe7dd3f2
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-02-16 19:40:48 +0500

    caching

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -58,7 +58,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: IsolateApiCall(),
+      home: AlbumsScreen(),
     );
   }
 }
